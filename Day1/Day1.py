c3ce848c1b4ba51f93dbac07dd13ef0b682d3fd5
Acellama88 <acellama88@gmail.com>
2022-11-30 20:15:16 -0600

    Testing Source Control

---
+++ Day1/Day1.py
@@ -0,0 +1,25 @@
+import re
+
+file = "./Day01/input.txt"
+finalTotal = 0
+input = []
+
+def parse():
+    #with open(file) as f:
+    #    for line in f:
+    #        if False:
+    #            input.extend(re.split("",line.strip()))
+    #        else:
+    #            input.append(line.strip())
+    pass
+
+def part1():
+    print("Hello")
+
+def part2():
+    print("World")
+
+if __name__ == '__main__':
+    parse()
+    part1()
+    part2()
