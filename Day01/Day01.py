d64c89f28b2166890ac18eab13aa0cffedcf3e75
Acellama88 <acellama88@gmail.com>
2022-11-30 23:13:22 -0600

    Day 01 Complete

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,6 +1,6 @@
 import re
 
-file = "./Day1/input.txt"
+file = "./Day01/input.txt"
 finalTotal = 0
 input = []
 
@@ -14,26 +14,31 @@ def parse():
             else:
                 input.append(line.strip())
 
-
 def part1():
     count = 0
-    last = input[0]
+    total = 0
     for x in input:
-        if last < x:
-            count = count + 1
-        last = x
-
+        if(x == ''):
+            if(total > count):
+                count = total
+            total = 0
+        else:
+            total = total + int(x)
     print(count)
 
 def part2():
-    count = 0
-    last = input[0]
-    for x in range(0,len(input)-3):
-        comp1 = input[x] + input[x+1] + input[x+2]
-        comp2 = input[x+1] + input[x+2] + input[x+3]
-        if(comp2 > comp1):
-            count = count + 1
-    print(count)
+    input.append('')
+    count = [0,0,0]
+    total = 0
+    for x in input:
+        if(x == ''):
+            count.append(total)
+            count.sort(reverse=True)
+            count.pop()
+            total = 0
+        else:
+            total = total + int(x)
+    print(count[0] + count[1] + count[2])
 
 if __name__ == '__main__':
     parse()
