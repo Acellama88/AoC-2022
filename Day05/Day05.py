53689b1330f20cf787c0f06b0485bc0ae294dbbf
acellama88 <acellama8@gmail.com>
2022-12-05 09:36:57 -0600

    Day 05 Complete

---
+++ Day05/Day05.py
@@ -0,0 +1,51 @@
+import re
+
+file = "./Day05/input.txt"
+finalTotal = 0
+input = []
+
+a1 = ['F', 'C', 'P', 'G', 'Q', 'R']
+a2 = ['W', 'T', 'C', 'P']
+a3 = ['B', 'H', 'P', 'M', 'C']
+a4 = ['L', 'T', 'Q', 'S', 'M', 'P', 'R']
+a5 = ['P', 'H', 'J', 'Z', 'V', 'G', 'N']
+a6 = ['D', 'P', 'J']
+a7 = ['L', 'G', 'P', 'Z', 'F', 'J', 'T', 'R']
+a8 = ['N', 'L', 'H', 'C', 'F', 'P', 'T', 'J']
+a9 = ['G', 'V', 'Z', 'Q', 'H', 'T', 'C', 'W']
+
+stacks = [a1, a2, a3, a4, a5, a6, a7, a8, a9]
+
+def parse():
+    with open(file) as f:
+        for line in f:
+            if False:
+                input.extend(re.split("",line.strip()))
+                input.remove("")
+            elif False:
+                input.append(int(line.strip()))
+            else:
+                input.append(line.strip())
+
+def part1():
+    for x in input:
+        tokens = re.split(" ",x)
+        for i in range(0,int(tokens[1])):
+            val = stacks[int(tokens[3])-1].pop()
+            stacks[int(tokens[5])-1].append(val)
+    print(stacks)
+
+def part2():
+    temp = []
+    for x in input:
+        tokens = re.split(" ",x)
+        for i in range(0,int(tokens[1])):
+            temp.append(stacks[int(tokens[3])-1].pop())
+        for i in range(0,int(tokens[1])):
+            stacks[int(tokens[5])-1].append(temp.pop())
+    print(stacks)
+
+if __name__ == '__main__':
+    parse()
+    #part1()
+    part2()
